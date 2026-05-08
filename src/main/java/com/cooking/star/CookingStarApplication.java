e0d0c78d9bc9ab63256fa1155fc34002f7a4de41
Taehyun-0502 <uiop72323@gmail.com>
2026-05-08 17:46:39 +0900

    test

---
+++ src/main/java/com/cooking/star/CookingStarApplication.java
@@ -0,0 +1,13 @@
+package com.cooking.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooking/star/CookingStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooking.star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
