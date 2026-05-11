0d1053abfc15dc3f4a0702a9e5b996de94357e17
Taehyun-0502 <uiop72323@gmail.com>
2026-05-11 11:36:57 +0900

    2026-05-11 memberDTO

---
+++ src/main/java/com/cooking/star/member/MemberDTO.java
@@ -0,0 +1,24 @@
+package com.cooking.star.member;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+	private String username;
+	private String password;
+	private String name;
+	private String follower;
+	private String following;
+	private String email;
+	private boolean enabled;
+	private boolean credentialsNonExpired;
+	private boolean accountNonLocked;
+	private boolean accountNonExpired;
+	
+	
+}
---
+++ src/main/java/com/cooking/star/member/ProfileDTO.java
@@ -0,0 +1,16 @@
+package com.cooking.star.member;
+
+import com.cooking.star.file.FileDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProfileDTO extends FileDTO{
+
+	private String username;
+	
+}
